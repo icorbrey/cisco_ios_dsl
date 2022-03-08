3166f6cfe86801eff364132e9f7b82bffdba43a1
Isaac <icorbrey@gmail.com>
2022-03-08 15:57:37 -0500

    feat: Create scope stack

---
+++ lib/src/util/scope.dart
@@ -0,0 +1,22 @@
+class Scope {
+  final String preamble;
+  final String label;
+
+  const Scope(this.label, this.preamble);
+
+  @override
+  bool operator ==(Object other) =>
+      other is Scope &&
+      other.runtimeType == runtimeType &&
+      other.label == label;
+
+  const Scope.root() : this('root', '>');
+  const Scope.dhcp() : this('dhcp', '(dhcp-config)#');
+  const Scope.line() : this('line', '(config-line)#');
+  const Scope.vlan() : this('vlan', '(config-vlan)#');
+  const Scope.priviledged() : this('priviledged', '#');
+  const Scope.configure() : this('configure', '(config)#');
+  const Scope.interface() : this('interface', '(config-if)#');
+  const Scope.subinterface() : this('subinterface', '(config-subif)#');
+  const Scope.interfaceRange() : this('interface-range', '(config-if-range)#');
+}
---
+++ lib/src/util/scope_stack.dart
@@ -0,0 +1,19 @@
+import 'package:cisco_ios_dsl/src/util/scope.dart';
+
+class ScopeStack {
+  final List<Scope> _stack;
+
+  ScopeStack() : _stack = [Scope.root()];
+
+  Scope get current => _stack.last;
+
+  void push(Scope scope) {
+    _stack.add(scope);
+  }
+
+  void pop() {
+    if (1 < _stack.length) {
+      _stack.removeLast();
+    }
+  }
+}
