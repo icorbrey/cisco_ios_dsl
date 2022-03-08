45ee4aa651a903d493bcc27cea07c628987932c6
Isaac <icorbrey@gmail.com>
2022-03-08 15:49:38 -0500

    feat: Create generic activator

---
+++ lib/src/util/activator.dart
@@ -0,0 +1,17 @@
+import "dart:mirrors";
+
+class Activator {
+  static createInstance(Type type,
+      {Symbol constructor = const Symbol(""),
+      List arguments = const [],
+      Map<Symbol, dynamic> namedArguments = const {}}) {
+    var typeMirror = reflectType(type);
+    if (typeMirror is ClassMirror) {
+      return typeMirror
+          .newInstance(constructor, arguments, namedArguments)
+          .reflectee;
+    } else {
+      throw ArgumentError("Cannot create the instance of the type '$type'.");
+    }
+  }
+}
