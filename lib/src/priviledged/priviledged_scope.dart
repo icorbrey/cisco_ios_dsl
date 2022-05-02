b5d7c77b75b6f9e5b65075074f3512036888facb
Isaac <icorbrey@gmail.com>
2022-05-02 19:25:32 -0400

    feat: Document priviledged scope

--- lib/src/priviledged/priviledged_scope.dart
+++ lib/src/priviledged/priviledged_scope.dart
@@ -3,20 +3,31 @@ import 'package:cisco_ios_dsl/src/device.dart';
 import 'package:cisco_ios_dsl/src/util/scope.dart';
 import 'package:intl/intl.dart';
 
+/// The Priviledged scope.
 class PriviledgedScope {
 
+  /// The device being configured.
   final Device device;
+
   PriviledgedScope(this.device);
 
+  /// Configures the device.
   void configure(void Function(ConfigurationScope x) body) => device
+    ..comment('Configure this device')
     ..useScope(Scope.configure(), 'configure terminal', body);
 
+  /// Sets the clock to [time]
   void setClock(DateTime time) => device
+    ..comment('Set the time to ${time.toLocal()}')
     ..run('clock set ${DateFormat("HH:mm:ss dd MMMM yyyy").format(time)}');
 
+  /// Saves the current configuration
   void saveConfig() => device
+    ..comment('Save the current configuration')
     ..run('copy running-config startup-config \n');
 
+  /// Reloads the device
   void reload() => device
+    ..comment('Reload the device')
     ..run('reload');
 }
