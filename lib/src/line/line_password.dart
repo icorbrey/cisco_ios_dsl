7dd06ed7a5f279ec01e5f30b1404d76491e2c976
Isaac <icorbrey@gmail.com>
2022-05-02 19:23:10 -0400

    feat: Document line scope

--- lib/src/line/line_password.dart
+++ lib/src/line/line_password.dart
@@ -1,14 +1,20 @@
 import 'package:cisco_ios_dsl/src/device.dart';
 
+/// The password configurator for the Line scope.
 class LinePassword {
 
+  /// The device being configured.
   final Device device;
 
   LinePassword(this.device);
 
+  /// Disables the password for this line.
   void disable() => device
+    ..comment('Disable the password for this line')
     ..run('no password');
 
+  /// Sets the [password] for this line.
   void enable(String password) => device
+    ..comment('Set this line\'s password')
     ..run('password $password');
 }
--- lib/src/line/line_transport_input.dart
+++ lib/src/line/line_transport_input.dart
@@ -1,14 +1,20 @@
 import 'package:cisco_ios_dsl/src/device.dart';
 
+/// The input transport method configurator for the Line scope.
 class LineTransportInput {
 
+  /// The device being configured.
   final Device device;
 
   LineTransportInput(this.device);
 
+  /// Disables the input transport for this line.
   void disable() => device
+    ..comment('Disable the input transport for this line')
     ..run('transport input none');
 
-  void enable(String type) => device
-    ..run('transport input $type');
+  /// Sets the input transport [method] for this line.
+  void enable(String method) => device
+    ..comment('Set the input transport method for this line')
+    ..run('transport input $method');
 }
